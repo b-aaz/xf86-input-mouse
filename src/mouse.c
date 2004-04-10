d7590d5a61f3058d358ea33c01e051139b4b46fc
Roland Mainz <roland.mainz@nrubsig.org>
2004-04-10 09:05:20 +0000

    Resync to 2004-04-10 XORG-RELEASE-1 branch

--- src/mouse.c
+++ src/mouse.c
@@ -1,4 +1,4 @@
-/* $XdotOrg: xc/programs/Xserver/hw/xfree86/input/mouse/mouse.c,v 1.1.4.2.4.1 2004/03/04 17:48:05 eich Exp $ */
+/* $XdotOrg: xc/programs/Xserver/hw/xfree86/input/mouse/mouse.c,v 1.1.4.2.4.4 2004/04/06 20:47:50 eich Exp $ */
 /* $XFree86: xc/programs/Xserver/hw/xfree86/input/mouse/mouse.c,v 1.79 2003/11/03 05:11:48 tsi Exp $ */
 /*
  *
@@ -3568,7 +3568,7 @@ static XF86ModuleVersionInfo xf86MouseVersionRec =
     MODULEVENDORSTRING,
     MODINFOSTRING1,
     MODINFOSTRING2,
-    XF86_VERSION_CURRENT,
+    XORG_VERSION_CURRENT,
     1, 0, 0,
     ABI_CLASS_XINPUT,
     ABI_XINPUT_VERSION,
