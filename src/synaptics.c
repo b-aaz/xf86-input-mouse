ae160fc87bd2d52e0ab807b31dc2682beb3db772
Alan Coopersmith <alan.coopersmith@oracle.com>
2022-01-16 13:04:35 -0800

    Convert remaining source files from ISO-8859-1 to UTF-8
    Most were already converted.  Fixes error raised by FlawFinder in CI:
    Error: encoding error in ./src/synaptics.c
    'utf-8' codec can't decode byte 0xa9 in position 16: invalid start byte
    
    Signed-off-by: Alan Coopersmith <alan.coopersmith@oracle.com>

--- src/synaptics.c
+++ src/synaptics.c
@@ -1,20 +1,20 @@
 /*
- * Copyright © 1999 Henry Davies
- * Copyright © 2001 Stefan Gmeiner
- * Copyright © 2002 S. Lehner
- * Copyright © 2002 Peter Osterlund
- * Copyright © 2002 Linuxcare Inc. David Kennedy
- * Copyright © 2003 Hartwig Felger
- * Copyright © 2003 Jörg Bösner
- * Copyright © 2003 Fred Hucht
- * Copyright © 2004 Alexei Gilchrist
- * Copyright © 2004 Matthias Ihmig
- * Copyright © 2006 Stefan Bethge
- * Copyright © 2006 Christian Thaeter
- * Copyright © 2007 Joseph P. Skudlarek
- * Copyright © 2008 Fedor P. Goncharov
- * Copyright © 2008-2012 Red Hat, Inc.
- * Copyright © 2011 The Chromium OS Authors
+ * Copyright Â© 1999 Henry Davies
+ * Copyright Â© 2001 Stefan Gmeiner
+ * Copyright Â© 2002 S. Lehner
+ * Copyright Â© 2002 Peter Osterlund
+ * Copyright Â© 2002 Linuxcare Inc. David Kennedy
+ * Copyright Â© 2003 Hartwig Felger
+ * Copyright Â© 2003 JÃ¶rg BÃ¶sner
+ * Copyright Â© 2003 Fred Hucht
+ * Copyright Â© 2004 Alexei Gilchrist
+ * Copyright Â© 2004 Matthias Ihmig
+ * Copyright Â© 2006 Stefan Bethge
+ * Copyright Â© 2006 Christian Thaeter
+ * Copyright Â© 2007 Joseph P. Skudlarek
+ * Copyright Â© 2008 Fedor P. Goncharov
+ * Copyright Â© 2008-2012 Red Hat, Inc.
+ * Copyright Â© 2011 The Chromium OS Authors
  *
  * Permission to use, copy, modify, distribute, and sell this software
  * and its documentation for any purpose is hereby granted without
@@ -41,7 +41,7 @@
  *      Stefan Bethge <stefan.bethge@web.de>
  *      Matthias Ihmig <m.ihmig@gmx.net>
  *      Alexei Gilchrist <alexei@physics.uq.edu.au>
- *      Jörg Bösner <ich@joerg-boesner.de>
+ *      JÃ¶rg BÃ¶sner <ich@joerg-boesner.de>
  *      Hartwig Felger <hgfelger@hgfelger.de>
  *      Peter Osterlund <petero2@telia.com>
  *      S. Lehner <sam_x@bluemail.ch>
